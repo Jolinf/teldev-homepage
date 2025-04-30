d3850158b8b3a8526b0574a532f5330cb90af076
Jolinf <joshuaulinfun@gmail.com>
2025-04-30 14:36:40 +0100

    Update README and Vite configuration for TelDev homepage project; added project overview, features, tech stack, and improved server settings.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server:{
-    allowedHosts:['5173-jolinf-teldevhomepage-uxndy62vj47.ws-eu118.gitpod.io']
+  server: {
+    allowedHosts: ['5173-jolinf-teldevhomepage-uxndy62vj47.ws-eu118.gitpod.io'],
   },
   css: {
     postcss: './postcss.config.js',
