0934fb2f1bfc069fb313a9bb38104a87cd62a416
Jolinf <joshuaulinfun@gmail.com>
2025-05-01 11:30:56 +0100

    whatweoffer link fix 2

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/teldev-homepage">
+    <BrowserRouter basename="/teldev-homepage/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
