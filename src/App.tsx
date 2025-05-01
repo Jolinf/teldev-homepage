c25465700839cff56aaaac5a98c9933dbb6813ab
Jolinf <joshuaulinfun@gmail.com>
2025-05-01 11:36:08 +0100

    page reset

--- src/App.tsx
+++ src/App.tsx
@@ -9,23 +9,27 @@ import Network from './Pages/Network';
 import Webdev from './Pages/Webdev';
 import Cloud from './Pages/Cloud';
 import ItConsulting from './Pages/ItConsulting';
+import ScrollToTop from './components/ScrollToTop';
 // import About from './Pages/About';
 // import Services from './Pages/services';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/whoweare" element={<WhoWeAre />} />
-      <Route path="/whatweoffer" element={<WhatWeOffer />} />
-      <Route path="/Helpdesk" element={<Helpdesk />} />
-      <Route path="/Network" element={<Network />} />
-      <Route path="/Webdev" element={<Webdev />} />
-      <Route path="/Cloud" element={<Cloud />} />
-      <Route path="/ItConsulting" element={<ItConsulting />} />
-      {/* <Route path="/about" element={<About />} />
-      <Route path="/services" element={<Services />} /> */}
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/whoweare" element={<WhoWeAre />} />
+        <Route path="/whatweoffer" element={<WhatWeOffer />} />
+        <Route path="/Helpdesk" element={<Helpdesk />} />
+        <Route path="/Network" element={<Network />} />
+        <Route path="/Webdev" element={<Webdev />} />
+        <Route path="/Cloud" element={<Cloud />} />
+        <Route path="/ItConsulting" element={<ItConsulting />} />
+        {/* <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} /> */}
+      </Routes>
+    </>
   );
 }
 
