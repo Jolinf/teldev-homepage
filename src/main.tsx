a66e04f93725b478fdb605c38d348d4847f183f7
Jolinf <joshuaulinfun@gmail.com>
2025-11-19 12:54:32 +0100

    speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ScrollProvider } from './contexts/ScrollContext';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter basename="/">
       <ScrollProvider>
+        <SpeedInsights />
         <App />
       </ScrollProvider>
     </BrowserRouter>
